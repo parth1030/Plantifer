d80d80ca397985590b4920e32ad67ea9d590e2d2
parthsharma <psharma7@uci.edu>
2025-08-21 12:36:36 -0700

    Initial Commit

---
+++ Plantifer/PlantiferApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantiferApp.swift
+//  Plantifer
+//
+//  Created by Parth Sharma on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct PlantiferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
